4c4292ee63b5c9d89db13afc9fcdbaab4c77b4b3
Hunger-Prevails <yinglun.liu.it.happens@gmail.com>
2026-06-18 16:26:52 +0200

    init

---
+++ apps/main.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include <numeric>
+# include <cxxopts.hpp>
+
+namespace fs = std::filesystem;
+
+int main(int argc, char *argv[]) {
+    cxxopts::Options options("marker-pose", "options to configure marker pose estimation pipeline");
+
+    options.add_options()("image", "Path to the image to process", cxxopts::value<fs::path>());
+    options.add_options()("marker-side", "Length of the marker's side in meters", cxxopts::value<float>()->default_value("10.0"));
+
+    auto args = options.parse(argc, argv);
+
+    return 0;
+}
